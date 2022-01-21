cc21889744cb87e71cf18597be953c996427fa19
lefebvredavid59 <dlefebvredev@gmail.com>
2022-01-21 13:56:10 +0100

    update view admin real categ

--- src/Form/CategBlogType.php
+++ src/Form/CategBlogType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\CategBlog;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +13,9 @@ class CategBlogType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('slug')
+            ->add('name',TextType::class,[
+                'label'=>'Nom de la catégorie Blog'
+            ])
         ;
     }
 
--- src/Form/CategRealType.php
+++ src/Form/CategRealType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\CategReal;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +13,9 @@ class CategRealType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('slug')
-        ;
+            ->add('name', TextType::class, [
+                'label' => 'Nom de la catégorie Blog'
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
