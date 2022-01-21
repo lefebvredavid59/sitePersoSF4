873863287e3ec00cf49b4d6421526cf03c0cc8e4
lefebvredavid59 <dlefebvredev@gmail.com>
2022-01-21 10:49:28 +0100

    first commit

---
+++ src/Form/SiteType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Site;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SiteType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('register')
+            ->add('available')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Site::class,
+        ]);
+    }
+}
